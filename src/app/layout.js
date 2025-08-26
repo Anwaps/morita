b329b48599b7f3425cd9629e75e032252065660e
Anwaps <vatvghjkl@gmail.com>
2025-08-26 10:44:20 +0700

    fix image 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export const metadata = {
     siteName: "ระบบดับเพลิง Morita Uehara Trading",
     images: [
       {
-        url: "/group-brand.webp", // รูปโลโก้หรือภาพ Hero
+        url: "https://morita-miyata.vercel.app/group-brand.webp?v=2", // รูปโลโก้หรือภาพ Hero
         width: 1200,
         height: 630,
       },
